2fb76fd7e12d1aa4079799279e9959922d276fe5
Bruno Rocha <rochacbruno@gmail.com>
2019-12-02 17:12:12 -0200

    Adiciona talks.json e a solução JS do  @giovanisleite
    O @giovanisleite resolveu o problema do JS :)
    
    Agora só vai faltar o CSS ficar bonito! :)

---
+++ static/js/talks.js
@@ -0,0 +1,63 @@
+data = {
+    "talks": [
+        {
+            "title": "Olá Mundo",
+            "datetime": "2019-12-02T16:40:00.000",
+            "duration": 14400,
+            "embed_link": "https://www.youtube.com/embed/3bIw6294Skk",
+            "link": "https://www.youtube.com/watch?v=3bIw6294Skk"
+        },
+        {
+            "title": "Olá Outro Mundo",
+            "datetime": "2019-12-02T16:30:15.000",
+            "duration": 14400,
+            "embed_link": "https://www.youtube.com/embed/3bIw6294Skk",
+            "link": "https://www.youtube.com/watch?v=3bIw6294Skk"
+        },
+    ]
+}
+
+function getTimeZonedDate(offset) {
+    // create Date object for current location
+    var d = new Date();
+
+    // convert to msec
+    // subtract local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+    // create new Date object for different city
+    // using supplied offset
+    var nd = new Date(utc + (3600000*offset));
+
+    return nd
+}
+
+function isHappeningNow(talk) {
+  //const now = new Date()
+  const now = getTimeZonedDate('-2')
+  const start = new Date(talk.datetime)
+  const durationInMs = talk.duration * 60000
+  const end = new Date(start.getTime() + durationInMs)
+  return start <= now && end >= now
+}
+
+function talkToHTML(talk) {
+    var talk_time = new Date(talk.datetime);
+    return `
+<div id="talk-${talk.title}" class="col-xl-5 offset-xl-1 col-md-5">
+    <h2> <a href="${talk.link}"> ${talk.title} </a></h2>
+    ${talk_time.toDateString()} - ${talk_time.toTimeString()}
+    <iframe
+    id="video"
+    width="560" 
+    height="315" 
+    src="${talk.embed_link}"
+    >
+    </iframe>
+</div>
+`
+}
+
+const talks = data.talks.filter(isHappeningNow)
+document.getElementById('talks').innerHTML = talks.map(talkToHTML).join('\n')
