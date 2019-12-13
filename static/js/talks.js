55987bdf8e02ac87b6c4b7319dc8194a2ed08dc3
Camila Maia <cmaiacd@gmail.com>
2019-12-13 13:48:13 -0300

    Adicionando todas as talks

--- static/js/talks.js
+++ static/js/talks.js
@@ -1,19 +1,348 @@
 data = {
     "talks": [
         {
-            "title": "Olá Mundo",
-            "datetime": "2019-12-13T16:00:00",
-            "duration": 14400,
-            "embed_link": "https://www.youtube.com/embed/3bIw6294Skk",
-            "link": "https://www.youtube.com/watch?v=3bIw6294Skk"
+            "datetime": "2019-12-13T16:00:00.00",
+            "title": "Abertura PyJamas Conf 2019",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=8wAmTH9hBac",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/8wAmTH9hBac"
         },
         {
-            "title": "Olá Outro Mundo",
-            "datetime": "2019-12-14T16:00:00",
-            "duration": 14400,
-            "embed_link": "https://www.youtube.com/embed/3bIw6294Skk",
-            "link": "https://www.youtube.com/watch?v=3bIw6294Skk"
+            "datetime": "2019-12-13T16:30:00.00",
+            "title": "Mantendo a documentação do Python em Português!",
+            "duration": 20,
+            "link": "https:\/\/www.youtube.com\/watch?v=ohpy_5ppoa0",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/ohpy_5ppoa0"
         },
+        {
+            "datetime": "2019-12-13T17:00:00.00",
+            "title": "Django em Docker",
+            "duration": 45,
+            "link": "https:\/\/www.youtube.com\/watch?v=s4shJGh-eY0",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/s4shJGh-eY0"
+        },
+        {
+            "datetime": "2019-12-13T17:00:00.00",
+            "title": "Live Coding: Entregando mais e mais rápido com o ERP de código aberto Odoo",
+            "duration": 60,
+            "link": "https:\/\/www.youtube.com\/watch?v=lwT9nEBdPR8",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/lwT9nEBdPR8"
+        },
+        {
+            "datetime": "2019-12-13T17:00:00.00",
+            "title": "Hello World: Criando uma infraestrutura escalável",
+            "duration": 30,
+            "link": "http:\/\/YouTube.com\/phibos01",
+            "embed_link": "http:\/\/YouTube.com\/phibos01"
+        },
+        {
+            "datetime": "2019-12-13T17:00:00.00",
+            "title": "Engenharia de Qualidade com Python",
+            "duration": 90,
+            "link": "https:\/\/www.youtube.com\/watch?v=WiRCxvZfzEY",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/WiRCxvZfzEY"
+        },
+        {
+            "datetime": "2019-12-13T18:00:00.00",
+            "title": "Arquitetura de um Projeto Flask",
+            "duration": 60,
+            "link": "https:\/\/YouTube.com\/CodeShowBR",
+            "embed_link": "https:\/\/YouTube.com\/CodeShowBR"
+        },
+        {
+            "datetime": "2019-12-13T18:00:00.00",
+            "title": "Criando bots pra telegram usando python",
+            "duration": 60,
+            "link": "https:\/\/www.youtube.com\/watch?v=QL1CcFOK0Uw",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/QL1CcFOK0Uw"
+        },
+        {
+            "datetime": "2019-12-13T18:00:00.00",
+            "title": "Conhecendo a biblioteca de Reconhecimento Facial linguagem Python",
+            "duration": 60,
+            "link": "https:\/\/www.youtube.com\/watch?v=iy3vXv62Jv4",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/iy3vXv62Jv4"
+        },
+        {
+            "datetime": "2019-12-13T19:00:00.00",
+            "title": "Informação Salva: Python ajudando a entender um problema silencioso de saúde pública no Brasil e no mundo, o Suicídio. ",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=dMplCkeqnUw",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/dMplCkeqnUw"
+        },
+        {
+            "datetime": "2019-12-13T19:00:00.00",
+            "title": "Teste de Hipóteses com Python",
+            "duration": 45,
+            "link": "https:\/\/www.youtube.com\/watch?v=O96fbdKrV1Q ",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/O96fbdKrV1Q "
+        },
+        {
+            "datetime": "2019-12-13T19:30:00.00",
+            "title": "Analisando queries do Django!",
+            "duration": 40,
+            "link": "https:\/\/youtube.com\/c\/RafaelGalleani ",
+            "embed_link": "https:\/\/youtube.com\/c\/RafaelGalleani "
+        },
+        {
+            "datetime": "2019-12-13T20:00:00.00",
+            "title": "Por que usamos Python?",
+            "duration": 50,
+            "link": "https:\/\/youtube.com\/renzoprobr",
+            "embed_link": "https:\/\/youtube.com\/renzoprobr"
+        },
+        {
+            "datetime": "2019-12-13T20:00:00.00",
+            "title": "De Engenheiro Senior a Diretor Em 7 Anos",
+            "duration": 20,
+            "link": "https:\/\/www.youtube.com\/watch?v=cLWWTdTYioY",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/cLWWTdTYioY"
+        },
+        {
+            "datetime": "2019-12-13T20:00:00.00",
+            "title": "Não seja apenas um programador",
+            "duration": 60,
+            "link": "https:\/\/www.youtube.com\/watch?v=DE-SACLASmk",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/DE-SACLASmk"
+        },
+        {
+            "datetime": "2019-12-13T21:00:00.00",
+            "title": "Introdução a Arquitetura do Django",
+            "duration": 80,
+            "link": "https:\/\/www.youtube.com\/c\/regis-do-python ",
+            "embed_link": "https:\/\/www.youtube.com\/c\/regis-do-python "
+        },
+        {
+            "datetime": "2019-12-13T21:00:00.00",
+            "title": "Escrevendo um bot do telegram em Python para a comunidade PythOnRio",
+            "duration": 50,
+            "link": "https:\/\/www.youtube.com\/watch?v=TII3iYvlmtw ",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/TII3iYvlmtw "
+        },
+        {
+            "datetime": "2019-12-13T22:00:00.00",
+            "title": "Automatizando tarefas com Python",
+            "duration": 60,
+            "link": "https:\/\/m.youtube.com\/channel\/UCG_GH22KoZYifrH7HJtoz4g ",
+            "embed_link": "https:\/\/m.youtube.com\/channel\/UCG_GH22KoZYifrH7HJtoz4g "
+        },
+        {
+            "datetime": "2019-12-13T22:00:00.00",
+            "title": "Estruturas de Dados (com python) são (mais) legais",
+            "duration": 40,
+            "link": "https:\/\/www.youtube.com\/watch?v=0vxoXu4FUEE ",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/0vxoXu4FUEE "
+        },
+        {
+            "datetime": "2019-12-13T23:00:00.00",
+            "title": "Python e os seus frameworks web",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=I-pk4q9JxOg",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/I-pk4q9JxOg"
+        },
+        {
+            "datetime": "2019-12-13T23:00:00.00",
+            "title": "Data Science: É de comer?",
+            "duration": 40,
+            "link": "https:\/\/www.youtube.com\/channel\/UCywxFahcIZtnrLFXgFuGHqw\/",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCywxFahcIZtnrLFXgFuGHqw\/"
+        },
+        {
+            "datetime": "2019-12-14T00:00:00.00",
+            "title": "Organizando sua análise de dados com Snakemake",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=fSF2_3fO8mE",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/fSF2_3fO8mE"
+        },
+        {
+            "datetime": "2019-12-14T00:01:00.00",
+            "title": "Django ORM vs SQLAlchemy",
+            "duration": 25,
+            "link": "https:\/\/www.youtube.com\/user\/huogerac",
+            "embed_link": "https:\/\/www.youtube.com\/user\/huogerac"
+        },
+        {
+            "datetime": "2019-12-14T01:00:00.00",
+            "title": "Desenvolvimento de aplicações web seguras",
+            "duration": 60,
+            "link": "https:\/\/www.youtube.com\/watch?v=fnbXXAWyoJA",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/fnbXXAWyoJA"
+        },
+        {
+            "datetime": "2019-12-14T02:00:00.00",
+            "title": "Transição profissional - front",
+            "duration": 20,
+            "link": "https:\/\/www.youtube.com\/watch?v=XgyMU5ztHZ4",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/XgyMU5ztHZ4"
+        },
+        {
+            "datetime": "2019-12-14T02:00:00.00",
+            "title": "Meu primeiro projeto usando TDD",
+            "duration": 50,
+            "link": "http:\/\/youtube.com\/c\/eduardomendes",
+            "embed_link": "http:\/\/youtube.com\/c\/eduardomendes"
+        },
+        {
+            "datetime": "2019-12-14T03:00:00.00",
+            "title": "Game Domain Development",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=EZuHhwU_O0o",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/EZuHhwU_O0o"
+        },
+        {
+            "datetime": "2019-12-14T04:00:00.00",
+            "title": "meu primeiro pacote python completo e profissional",
+            "duration": 60,
+            "link": "https:\/\/www.youtube.com\/watch?v=iYHnO4JRcaA",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/iYHnO4JRcaA"
+        },
+        {
+            "datetime": "2019-12-14T06:00:00.00",
+            "title": "Python em 20 minutos",
+            "duration": 20,
+            "link": "https:\/\/www.youtube.com\/channel\/UCv6KLlnNe4TdV_FEDN_iIYw",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCv6KLlnNe4TdV_FEDN_iIYw"
+        },
+        {
+            "datetime": "2019-12-14T06:00:00.00",
+            "title": " Inserção das Mulheres na TI nos últimos 10 anos - Análise de dados com Pandas",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=xe1qr_Bhfzs",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/xe1qr_Bhfzs"
+        },
+        {
+            "datetime": "2019-12-14T07:00:00.00",
+            "title": "Criando um bot no discord com Python ",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/channel\/UCv6KLlnNe4TdV_FEDN_iIYw",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCv6KLlnNe4TdV_FEDN_iIYw"
+        },
+        {
+            "datetime": "2019-12-14T07:00:00.00",
+            "title": "Python Com Delphi CE",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=-gWdoPA5mmo",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/-gWdoPA5mmo"
+        },
+        {
+            "datetime": "2019-12-14T08:00:00.00",
+            "title": "Python no QGIS",
+            "duration": 40,
+            "link": "https:\/\/www.youtube.com\/watch?v=3JW2hCAssuw",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/3JW2hCAssuw"
+        },
+        {
+            "datetime": "2019-12-14T08:00:00.00",
+            "title": "Automação de Testes na Prática com Python Selenium",
+            "duration": 40,
+            "link": "https:\/\/m.youtube.com\/channel\/UCB5vNpKjQLDbcxAZht8ombg",
+            "embed_link": "https:\/\/m.youtube.com\/channel\/UCB5vNpKjQLDbcxAZht8ombg"
+        },
+        {
+            "datetime": "2019-12-14T09:00:00.00",
+            "title": "Function as a Service no OpenFaaS com Docker e Python",
+            "duration": 60,
+            "link": "https:\/\/www.twitch.tv\/pokemaobr",
+            "embed_link": "https:\/\/www.twitch.tv\/pokemaobr"
+        },
+        {
+            "datetime": "2019-12-14T09:00:00.00",
+            "title": "Um dia, um código em Python vai corrigir uma redação do Enem",
+            "duration": 15,
+            "link": "https:\/\/www.youtube.com\/watch?v=Q23LYtmgrMs",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/Q23LYtmgrMs"
+        },
+        {
+            "datetime": "2019-12-14T10:00:00.00",
+            "title": "Nuitka - E se Python fosse compilada?",
+            "duration": 30,
+            "link": "https:\/\/youtu.be\/IYv4rSOD5Tw",
+            "embed_link": "https:\/\/youtu.be\/IYv4rSOD5Tw"
+        },
+        {
+            "datetime": "2019-12-14T10:00:00.00",
+            "title": "Desenvolvendo de interfaces gráficas em PyQt5",
+            "duration": 50,
+            "link": "https:\/\/www.youtube.com\/channel\/UCL1-jSyABixXuBmVyfUHxrw ",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCL1-jSyABixXuBmVyfUHxrw "
+        },
+        {
+            "datetime": "2019-12-14T10:00:00.00",
+            "title": "Python + Power BI, uma análise sobre o ENEM 2018",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=VlnJNaie_eg",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/VlnJNaie_eg"
+        },
+        {
+            "datetime": "2019-12-14T11:00:00.00",
+            "title": "Fazendo Analise de dados SÓ com Python (Just Python)",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=38-Oz8CocIQ",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/38-Oz8CocIQ"
+        },
+        {
+            "datetime": "2019-12-14T11:00:00.00",
+            "title": "Rodando uma API com Django Rest Framework no Google Cloud",
+            "duration": 40,
+            "link": "https:\/\/www.youtube.com\/user\/alvarofellipe",
+            "embed_link": "https:\/\/www.youtube.com\/user\/alvarofellipe"
+        },
+        {
+            "datetime": "2019-12-14T11:00:00.00",
+            "title": "Como o maior Django Girls do mundo mudou a minha vida",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/channel\/UCjXL44YbiOb1hdSwVq51VCw",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCjXL44YbiOb1hdSwVq51VCw"
+        },
+        {
+            "datetime": "2019-12-14T12:00:00.00",
+            "title": "Explorando Dados Com Python e Pandas",
+            "duration": 45,
+            "link": "https:\/\/www.youtube.com\/channel\/UCOF4kWbxCbAwm1v8yHlbpRw",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCOF4kWbxCbAwm1v8yHlbpRw"
+        },
+        {
+            "datetime": "2019-12-14T12:00:00.00",
+            "title": "Mudar de área aos 40. Pode isso Arnaldo?",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=EwijrYOyunw",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/EwijrYOyunw"
+        },
+        {
+            "datetime": "2019-12-14T13:00:00.00",
+            "title": "ScanAPI: Automatização de Testes de Integração para a sua API",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=aZ2Ry5znaDs",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/aZ2Ry5znaDs"
+        },
+        {
+            "datetime": "2019-12-14T14:00:00.00",
+            "title": "Gerando uma API automaticamente",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/channel\/UCI76AtUUJGThQx9p4Ke97wg",
+            "embed_link": "https:\/\/www.youtube.com\/channel\/UCI76AtUUJGThQx9p4Ke97wg"
+        },
+        {
+            "datetime": "2019-12-14T14:00:00.00",
+            "title": "Anti-patterns - o que são e como se livrar deles em Python",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=4DdrT4OXbVw",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/4DdrT4OXbVw"
+        },
+        {
+            "datetime": "2019-12-14T15:00:00.00",
+            "title": "Micro-serviço em 7 linhas!",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=0EI0wV5AZ4w",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/0EI0wV5AZ4w"
+        },
+        {
+            "datetime": "2019-12-14T16:00:00.00",
+            "title": "Encerramento Pyjamas Conf 2019",
+            "duration": 30,
+            "link": "https:\/\/www.youtube.com\/watch?v=hS4eLR1OY4M",
+            "embed_link": "https:\/\/www.youtube.com\/embed\/hS4eLR1OY4M"
+        }
     ]
 }
 
